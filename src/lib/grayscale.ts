0d643a37220d6457314e48223ef39f4f2de75248
Kangming Deng <dengkangming@gmail.com>
2026-08-20 01:45:59 -0400

    feat: add more grayscale filters

--- src/lib/grayscale.ts
+++ src/lib/grayscale.ts
@@ -1,5 +1,19 @@
 import tgpu, { d } from "typegpu";
-import { dot } from "typegpu/std";
+import { dot, max, min } from "typegpu/std";
+
+export const average = tgpu.fn(
+  [d.vec3f],
+  d.vec3f,
+)((linear) => d.vec3f((linear.r + linear.g + linear.b) / 3));
+
+export const lightness = tgpu.fn(
+  [d.vec3f],
+  d.vec3f,
+)((linear) =>
+  d.vec3f(
+    (max(linear.r, linear.g, linear.b) + min(linear.r, linear.g, linear.b)) / 2,
+  ),
+);
 
 /**
  * @see https://en.wikipedia.org/wiki/Luma_(video)#Rec._601_luma_versus_Rec._709_luma_coefficients
@@ -7,9 +21,7 @@ import { dot } from "typegpu/std";
 export const rec601 = tgpu.fn(
   [d.vec3f],
   d.vec3f,
-)((linear) => {
-  return d.vec3f(dot(linear, d.vec3f(0.299, 0.587, 0.114)));
-});
+)((linear) => d.vec3f(dot(linear, d.vec3f(0.299, 0.587, 0.114))));
 
 /**
  * @see https://en.wikipedia.org/wiki/Luma_(video)#Rec._601_luma_versus_Rec._709_luma_coefficients
@@ -17,6 +29,12 @@ export const rec601 = tgpu.fn(
 export const rec709 = tgpu.fn(
   [d.vec3f],
   d.vec3f,
-)((linear) => {
-  return d.vec3f(dot(linear, d.vec3f(0.2126, 0.7152, 0.0722)));
-});
+)((linear) => d.vec3f(dot(linear, d.vec3f(0.2126, 0.7152, 0.0722))));
+
+/**
+ * @see https://en.wikipedia.org/wiki/Rec._2020
+ */
+export const rec2020 = tgpu.fn(
+  [d.vec3f],
+  d.vec3f,
+)((linear) => d.vec3f(dot(linear, d.vec3f(0.2627, 0.678, 0.0593))));
